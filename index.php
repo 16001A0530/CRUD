1ffac2f4bd6ddbea9bfb588877d2c63aee1749f7
Venkatesh Pothagani <MyGitHub530@gmail.com>
2019-03-21 01:21:26 +0530

    Now data can read from db(index.php completed)

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php require_once("header.php") ?>
 <?php require_once("dataBase.php") ?>
 <?php
-    $query = "SELECT RollNumber, Name, Email, PhoneNo FROM Details WHERE Id = ?";
+    $roll = "";
+    $name = "";
+    $email = "";
+    $phone = "";
+    $query = "SELECT RollNumber, StudentName, Email, PhoneNo FROM Details";
     $statement = $connection->prepare($query);
     if($statement) {
-        $statement->bind_param("s", $rollNumber);
         if($statement->execute()) {
-            $statement->bind_result($roll);
-            while($statement->fetch()) {
-                if($rollNumber === $roll) {
-                   $rollCheck = "Roll Number must be UNIQUE."; 
-                }
-            }
+            $statement->bind_result($roll, $name, $email, $phone);
+            ;
+            
         }
     }
 ?>
@@ -22,6 +22,29 @@
             <h2 class="text-center" >Student Details</h2>
         </div>
         <div class="card-body">
+            <table class="table table-bordered" >
+                <tr>
+                    <th>Roll Number</th>
+                    <th>Name</th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                    <th>Action</th>
+                </tr>
+                
+                    <?php while($statement->fetch()): ?>
+                    <tr>
+                        <td><?php echo $roll ?></td>
+                        <td><?php echo $name ?></td>
+                        <td><?php echo $phone ?></td>
+                        <td><?php echo $email ?></td>
+                        <td>
+                            <a href="edit.php?rollNumber=<?php echo $roll ?>" class="ml-2 btn btn-light btn-sm active" role="button">Edit Details</a>
+                            <a href="delete.php?rollNumber=<?php echo $roll ?>" class="ml-2 btn btn-light btn-sm active" role="button">Delete</a>
+                        </td>
+                    </tr>
+                    <?php endwhile; ?>
+               
+            </table>
         </div>
     </div>
   </div>
